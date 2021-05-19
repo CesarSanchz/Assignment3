a57d13482a12e685ede24d30ab21c9478348e2ff
cesarsanchez <cesarsanchez@DESKTOP-JQV67KS.home>
2021-05-18 18:26:07 -0700

    All files for peer-review

---
+++ cst438Assignment3/src/main/java/cst438Assignment3/domain/CountryRepository.java
@@ -0,0 +1,8 @@
+package cst438Assignment3.domain;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, String> {
+    Country findByCode(String code);
+}
